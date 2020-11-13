64bbb4e15d9bdf4780047480ef2bab143f6ce501
J-jaeyoung <jjy600901@gmail.com>
2020-11-12 16:44:11 -0800

    uname syscall

--- hostname/gethostname.c
+++ hostname/gethostname.c
@@ -2,9 +2,14 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
+#include <sys/utsname.h>
 
 int main() {
     char buf[80];
-    gethostname(buf,sizeof(buf));
-    mkdir(buf, 0755);
+    struct utsname uts = {};
+    uname(&uts);
+    printf("%s\n",uts.nodename);
+    mkdir(uts.nodename, 0755);
 }
